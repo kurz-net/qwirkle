c4da42fd7f474b9a5ddf987bc846d7c7d8ae238d
Daniel Kurz <danielkurz03@protonmail.com>
2022-06-06 02:12:16 +0200

    add core types

---
+++ src/core/data.ts
@@ -0,0 +1,26 @@
+
+export type Color =
+  "red" | "blue" | "green" | "yellow" | "orange" | "violet"
+
+export type Shape =
+  "circle" | "flower" | "octothorb" | "spiked" | "square" | "star"
+
+export type Tile = {
+  uuid: string,
+  color: Color,
+  shape: Shape
+}
+
+export type Player = {
+  uuid: string,
+  name: string,
+  tiles: Tile[],
+  points: number
+}
+
+export type Game = {
+  uuid: string,
+  turn: number,
+  players: Player[],
+  tiles: Tile[]
+}
